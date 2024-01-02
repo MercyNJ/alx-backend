83f5e31ee6c3b7d5c31c7bb07d19a616c6133c55
MercyNJ <mercinjuguna@gmail.com>
2024-01-02 11:44:15 +0000

    MRU Caching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+MRUCache Module
+
+This module defines a MRUCache class
+that inherits from BaseCaching.
+It implements a caching system using the
+Most Recently Used (MRU) algorithm.
+"""
+
+from collections import OrderedDict
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """MRUCache class that implements MRU algorithm."""
+
+    def __init__(self):
+        """Initialize MRUCache."""
+        super().__init__()
+        self.used = []
+
+    def put(self, key, item):
+        """Add an item to the cache using MRU algorithm."""
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if key not in self.used:
+                self.used.append(key)
+            else:
+                self.used.append(
+                    self.used.pop(self.used.index(key)))
+            if len(self.used) > BaseCaching.MAX_ITEMS:
+                discard = self.used.pop(-2)
+                del self.cache_data[discard]
+                print(f'DISCARD: {discard}')
+
+    def get(self, key):
+        """Retrieve an item from the cache."""
+        if key is not None and key in self.cache_data:
+            self.used.append(self.used.pop(self.used.index(key)))
+            return self.cache_data.get(key)
+        return None
