797d816c805175bcbcf8ed8a5389dfd5053c5931
MercyNJ <mercinjuguna@gmail.com>
2024-01-03 13:30:31 +0000

    least frequency used caching algorithm

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -18,50 +18,44 @@ BaseCaching = __import__('base_caching').BaseCaching
 class LFUCache(BaseCaching):
     """LFUCache class that inherits from
     BaseCaching and implements LFU algorithm."""
-
     def __init__(self):
-        """Initialize LFUCache."""
+        """
+        Initialzation.
+        """
         super().__init__()
-        self.frequency_counter = defaultdict(int)
-        self.usage_order = OrderedDict()
+        self.order = OrderedDict()
+        self.frequency = {}
 
     def put(self, key, item):
-        """Add an item to the cache using LFU algorithm."""
+        """
+        Add item to cache.
+        """
         if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
+                    and key not in self.cache_data.keys():
+                least_freq_items = [k for k, v in self.frequency.items()
+                                    if v == min(self.frequency.values())]
+
+                discarded_key = min(
+                        least_freq_items, key=lambda k: self.order[k])
+
+                del self.cache_data[discarded_key]
+                del self.frequency[discarded_key]
+                del self.order[discarded_key]
+                print("DISCARD: {}".format(discarded_key))
+
             self.cache_data[key] = item
-            self.update_frequency_counter(key)
-            self.manage_cache_size()
+            self.frequency[key] = self.frequency.get(key, 0) + 1
+            self.order[key] = max(self.order.values(), default=-1) + 1
 
     def get(self, key):
-        """Retrieve an item from the cache."""
-        if key is not None and key in self.cache_data:
-            self.update_frequency_counter(key)
-            return self.cache_data[key]
-        return None
+        """
+        Get item by its key.
+        """
+        if key is None or key not in self.cache_data:
+            return None
 
-    def update_frequency_counter(self, key):
-        """Update the frequency counter and usage order for the given key."""
-        self.frequency_counter[key] += 1
-        if key in self.usage_order:
-            del self.usage_order[key]
-        self.usage_order[key] = None
+        self.order[key] = max(self.order.values(), default=-1) + 1
+        self.frequency[key] = self.frequency.get(key, 0) + 1
 
-    def manage_cache_size(self):
-        """Manage the size of the cache based on LFU algorithm."""
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            least_frequent_keys = [k for k, v in self.frequency_counter.items()
-                                   if v == min(self.frequency_counter.values())
-                                   ]
-
-            if len(least_frequent_keys) > 1:
-                least_recently_used_key = min(
-                        self.usage_order, key=lambda k: (
-                            self.usage_order[k], k), default=None)
-                discarded_key = least_recently_used_key
-            else:
-                discarded_key = least_frequent_keys[0]
-
-            del self.cache_data[discarded_key]
-            del self.frequency_counter[discarded_key]
-            del self.usage_order[discarded_key]
-            print('DISCARD: {:s}'.format(discarded_key))
+        return self.cache_data[key]
