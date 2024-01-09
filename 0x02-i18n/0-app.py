64677b83850a8e3051631d2edc68280a5ed57775
MercyNJ <mercinjuguna@gmail.com>
2024-01-09 09:20:54 +0000

    A basic flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app with single route.
+"""
+
+from flask import Flask, render_template
+
+app = Flak(__name__)
+
+
+@app.route('/')
+def index():
+    """
+    Route handle for root url.
+    """
+    return render_template('0-index.html')
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
