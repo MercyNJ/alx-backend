5961967e90aeff896520307c48c7144c843f5d61
MercyNJ <mercinjuguna@gmail.com>
2024-02-06 13:06:30 +0000

    stock management functionality

---
+++ 0x03-queuing_system_in_js/9-stock.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const redis = require('redis');
+const { promisify } = require('util');
+
+const app = express();
+const port = 1245;
+
+const listProducts = [
+  { itemId: 1, itemName: 'Suitcase 250', price: 50, initialAvailableQuantity: 4 },
+  { itemId: 2, itemName: 'Suitcase 450', price: 100, initialAvailableQuantity: 10 },
+  { itemId: 3, itemName: 'Suitcase 650', price: 350, initialAvailableQuantity: 2 },
+  { itemId: 4, itemName: 'Suitcase 1050', price: 550, initialAvailableQuantity: 5 }
+];
+
+function getItemById(id) {
+  return listProducts.find(product => product.itemId === id);
+}
+
+const client = redis.createClient();
+const getAsync = promisify(client.get).bind(client);
+
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error.message}`);
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+async function reserveStockById(itemId, stock) {
+  await client.set(`item.${itemId}`, stock);
+}
+
+async function getCurrentReservedStockById(itemId) {
+  const stock = await getAsync(`item.${itemId}`);
+  return stock ? parseInt(stock) : 0;
+}
+
+app.get('/list_products', (req, res) => {
+  res.json(listProducts);
+});
+
+app.get('/list_products/:itemId', async (req, res) => {
+  const itemId = parseInt(req.params.itemId);
+  const product = getItemById(itemId);
+  if (product) {
+    const currentQuantity = await getCurrentReservedStockById(itemId);
+    res.json({ ...product, currentQuantity });
+  } else {
+    res.status(404).json({ status: 'Product not found' });
+  }
+});
+
+app.get('/reserve_product/:itemId', async (req, res) => {
+  const itemId = parseInt(req.params.itemId);
+  const product = getItemById(itemId);
+  if (!product) {
+    res.status(404).json({ status: 'Product not found' });
+  } else if (product.initialAvailableQuantity === await getCurrentReservedStockById(itemId)) {
+    res.json({ status: 'Not enough stock available', itemId });
+  } else {
+    await reserveStockById(itemId, await getCurrentReservedStockById(itemId) + 1);
+    res.json({ status: 'Reservation confirmed', itemId });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
